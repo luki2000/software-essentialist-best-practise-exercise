a280dc4c40696704a2d1c898387d44617aee3ec8
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-07 20:57:12 +0200

    [New] function correctly returns fizzbuzz when input is both multiple of 5 and 3

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,8 +1,3 @@
-/**
- * Write a function that takes numbers from 1 to 100 and outputs them as a string,
- * but for multiples of three it returns “Fizz” instead of the number, and for multiples
- * of five it returns “Buzz.” For numbers that are multiples of both three and five, it returns “FizzBuzz.”
-*/
 export const FIZZ = 'Fizz' as const;
 export const BUZZ = 'Buzz' as const;
 export const FIZZBUZZ = 'fizzBUZZ' as const;
@@ -11,6 +6,7 @@ export function fizzBuzz(num: number) {
     const isMultipleOfThree = num % 3 === 0;
     const isMultipleOfFive = num % 5 === 0;
 
+    if (isMultipleOfThree && isMultipleOfFive) return FIZZBUZZ;
     if (isMultipleOfThree) return FIZZ;
     if (isMultipleOfFive) return BUZZ;
 }
