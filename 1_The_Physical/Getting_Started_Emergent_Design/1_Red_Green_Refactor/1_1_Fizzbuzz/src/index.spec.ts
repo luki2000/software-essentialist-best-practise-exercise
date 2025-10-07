44d2b08107e4846e5a8178276193f71e0f606f89
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-07 18:46:39 +0200

    [New] created first test, fizzbuzz function missing

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,21 @@
+/**
+ * Write a function that takes numbers from 1 to 100 and outputs them as a string,
+ * but for multiples of three it returns “Fizz” instead of the number, and for multiples
+ * of five it returns “Buzz.” For numbers that are multiples of both three and five, it returns “FizzBuzz.”
+*/
+
+/**
+ * 1. function receives a number and returns string
+ * 2. numbers it can accept are 1 to 100
+ * 3. if it can be divided by 3 and should return string Fizz
+ * 4. if it can be divided by 5 t should return buzz
+ * 5. if it can be divided by BOTH 3 and 5 and should return FizzBuzz
+ */
+
 
 describe("fizzbuzz", () => {
 
-
+    it('should return a string', () => {
+        expect(fizzbuzz(5)).toBe(typeof "string");
+    });
 });
