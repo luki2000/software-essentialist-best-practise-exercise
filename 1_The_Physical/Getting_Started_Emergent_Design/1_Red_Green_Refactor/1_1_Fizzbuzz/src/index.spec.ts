9c4e4ee6989ac57e696de0b80aae65576588b079
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-08 18:28:05 +0200

    [Refactor] used it.each to greatly reduce duplication

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,62 +2,48 @@ import { BUZZ, FIZZBUZZ, fizzBuzz } from './fizzbuzz';
 import { FIZZ } from './fizzbuzz';
 
 describe("fizzBuzz", () => {
-    let five: number;
-    let three: number;
 
-    beforeEach(() => {
-        three = 3;
-        five = 5;
+    it.each([{
+        input: 3,
+        expected: "string"
+    }])('should return a string', ({ input, expected }) => {
+        expect(typeof fizzBuzz(input)).toBe(expected);
     });
 
-    it('should return a string', () => {
-        const input = three;
-        expect(typeof fizzBuzz(input)).toBe("string");
+    it.each([
+         { input: 3, expected: FIZZ },
+         { input: 42, expected: FIZZ },
+         { input: 9, expected: FIZZ },
+    ])('should return a Fizz if number is a multiple of 3', ({input, expected}) => {
+        expect(fizzBuzz(input)).toBe(expected);
     });
 
-    it('should return a Fizz if number is a multiple of 3', () => {
-        const input = three;
-        expect(fizzBuzz(input)).toBe(FIZZ);
+    it.each([
+         { input: 5, expected: BUZZ },
+    ])('should return a Buzz if number is a multiple of 5', ({input, expected}) => {
+        expect(fizzBuzz(input)).toBe(expected);
     });
 
-    it('should return a Fizz if number is a multiple of 5', () => {
-        const input = five;
-        expect(fizzBuzz(input)).toBe(BUZZ);
-    });
-
-    it('should return a FizzBuzz if number is a multiple of both 5 and 3', () => {
-        const input = three * five;
-        expect(fizzBuzz(input)).toBe(FIZZBUZZ);
-    });
-
-    it('should return a Fizz if number is 9', () => {
-        expect(fizzBuzz(9)).toBe(FIZZ);
-    });
-
-    it('should return a Fizz if number is 42', () => {
-        expect(fizzBuzz(42)).toBe(FIZZ);
-    });
-
-    it('should return a Fizz if number is 45', () => {
-        expect(fizzBuzz(45)).toBe(FIZZBUZZ);
+    it.each([
+         { input: 15, expected: FIZZBUZZ },
+         { input: 45, expected: FIZZBUZZ },
+    ])('should return a FizzBuzz if number is a multiple of both 5 and 3', ({input, expected}) => {
+        expect(fizzBuzz(input)).toBe(expected);
     });
 
     it('should return a "43" if number is 43', () => {
         expect(fizzBuzz(43)).toBe("43");
     });
 
-    describe('it should throw error', () => {
-        it('for numbers above 100', () => {
-            expect(() => fizzBuzz(102)).toThrow('input number out of bounds');
-        });
+    it.each([
+         { input: 102, expected: 'input number out of bounds' },
+         { input: -12, expected: 'input number out of bounds' },
+    ])('should throw error when number is out of bounds, less than 1 and greater than 100', ({input, expected}) => {
+        expect(() => fizzBuzz(input)).toThrow(expected);
+    });
 
-        it('for numbers below 1', () => {
-            expect(() => fizzBuzz(-12)).toThrow('input number out of bounds');
-        });
-
-        it('if input is not a number', () => {
-            // force type casting to undefined, method only accepts numbers
-            expect(() => fizzBuzz(undefined as any)).toThrow('input not a number');
-        });
+    it('if input is not a number', () => {
+        // force type casting to undefined, method only accepts numbers
+        expect(() => fizzBuzz(undefined as any)).toThrow('input not a number');
     });
 });
