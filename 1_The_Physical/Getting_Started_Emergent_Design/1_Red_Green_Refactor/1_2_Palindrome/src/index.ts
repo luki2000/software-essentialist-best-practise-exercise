5937bca5dc71c7c9a8edfe5e775656c777043cdd
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-09 20:34:41 +0200

    [Green] using if conditions to return true to matching input text of the tests

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,7 @@
 export class Palindrome {
-    checker(text: string) {
-        return text === "mom";
+    checker(text: string): boolean {
+        if(text === "mom") return true;
+        if(text === "Mom") return true;
+        return false;
     }
 }
