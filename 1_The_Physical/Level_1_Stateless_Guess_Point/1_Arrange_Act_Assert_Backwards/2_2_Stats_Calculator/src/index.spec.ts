7ec9ffea25156817dffe0c9dad5a80b72a6a4a43
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-15 15:15:44 +0200

    upfront design

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -4,13 +4,17 @@
  Your task is to process a sequence of integer numbers to determine the following statistics:
  Without using system Math library functions, process a sequence of integers to determine the following statistics:
 
-For example: [2, 4, 21, -8, 53, 40]
+ For example: [2, 4, 21, -8, 53, 40]
 
-minimum value = -8, maximum value = 53, number of elements in the sequence = 6, average value = 18.666666666667
-
+ minimum value = -8, maximum value = 53, number of elements in the sequence = 6, average value = 18.666666666667
  * 
- * 
- * 
+ * input: an array of numbers
+ * output: an object wih calc stats 
+ * responsabilities
+ * 1. find minimum value
+ * 2. find maximum value
+ * 3. calc number of elements
+ * 4. calc average value
  * 
  */
 
