c8d6bb599c9c78a3df19ee982e4ea9bd9eb245a1
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-15 16:16:59 +0200

    [Green] findMaxNumber returns highest number

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,4 +1,20 @@
-export function findMaxNumber(arr: Array<number>): number {
+export function findMaxNumber(arr: Array<number>): number | null {
+    // 1 take full list of numbers
+    // 2 loop over each number number
+    // 3 set that number to a variable if it is larger than the previous number or if no number present, we start 
+    // 4 return variable
 
-    return 0;
+    let maxNumber = null;
+
+    for (const element of arr) {
+        // maxNumber is element if it is empty
+        if(maxNumber === null) {
+            maxNumber = element;
+        } else {
+        // maxnumber is element if its greater than maxNumber
+        maxNumber = element > maxNumber ? element : maxNumber;
+        }        
+    }
+
+    return maxNumber;
 }
