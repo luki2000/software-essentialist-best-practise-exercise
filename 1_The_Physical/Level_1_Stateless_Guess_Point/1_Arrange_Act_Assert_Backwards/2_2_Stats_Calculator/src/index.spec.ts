16f9865d8cdb77b7e1c18f348113b0468f9d28d7
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-15 15:07:50 +0200

    upfront design

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,3 +1,19 @@
+/**
+ * 
+ * 
+ Your task is to process a sequence of integer numbers to determine the following statistics:
+ Without using system Math library functions, process a sequence of integers to determine the following statistics:
+
+For example: [2, 4, 21, -8, 53, 40]
+
+minimum value = -8, maximum value = 53, number of elements in the sequence = 6, average value = 18.666666666667
+
+ * 
+ * 
+ * 
+ * 
+ */
+
 
 describe('stats calculator', () => {
 
