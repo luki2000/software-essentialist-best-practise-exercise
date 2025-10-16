517876af0b4e2f5b4dfaf9e6a7556de1a7affa04
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-16 23:39:43 +0200

    [Refactor] refactored test to remove duplication, and added more examples to test

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,27 +1,14 @@
 import { validateMilitaryTime } from './index';
 
-// Description: Write a function (or a stateless class) capable of validating whether a string time range is a 
-// valid military time range or not.
-
-// what is military time?
-// its time represented as day in hours (24h) so from 00:00 to 23:59
-// or function will have the time string as input and respond in boolean wether it is valid or not
-// each string time input as a "from" and "to" time seperated by a dash, both times need validation
-
-// Here are some string examples.
-
-// "01:12 - 14:32" (yes)
-// "25:00 - 12:23" (no)
-// "22:00 - 23:12" (yes)
-
-
-
 describe('military time validator', () => {
-    it('should know that "25:00 - 12:23" is not a valid military time', () => {
+    it.each([
+        { input: "25:00 - 12:23", expected : false },
+        { input: "22:00 - 23:12", expected : true },
+        { input: "22:00 - 23:12", expected : true },
+        { input: "60:00 - 23:90", expected : false },
+        { input: "00:00 - 23:59", expected : true },
+    ])('should know if $input is $expected as valid military time', () => {
         expect(validateMilitaryTime("25:00 - 12:23")).toBeFalsy();
     });
 
-    it('should know that "01:12 - 12:32" is valid military time', () => {
-        expect(validateMilitaryTime("01:12 - 12:32")).toBeTruthy();
-    });
 });
