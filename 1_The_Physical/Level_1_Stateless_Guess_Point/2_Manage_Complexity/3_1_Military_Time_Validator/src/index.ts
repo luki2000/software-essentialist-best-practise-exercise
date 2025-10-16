4c4395fa6de150ff3b10e10ab83b09d5296674b4
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-16 23:01:03 +0200

    [Red] split the two times and fake valid times by just checking if they are strings

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,4 +1,9 @@
 export function validateMilitaryTime(militaryTime: string) {
+    const splitMilitaryTime = militaryTime.split(" - ");
+    const fromTime = splitMilitaryTime[0];
+    const toTime = splitMilitaryTime[1];
     
-    return true;
+    const isFromTimeValid = typeof fromTime === "string";
+    const isToTimeValid = typeof toTime === "string";
+    return isFromTimeValid && isToTimeValid;
 }
