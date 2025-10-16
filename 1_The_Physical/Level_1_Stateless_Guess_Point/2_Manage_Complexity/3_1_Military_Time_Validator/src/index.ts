96e21c46377c31ff6863b452623d63b71e95295c
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-16 23:15:10 +0200

    [Green] added implmentation, split responsabilities of validation by creating one function that validates hour and another that validaes minutes

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,8 +2,29 @@ export function validateMilitaryTime(militaryTime: string) {
     const splitMilitaryTime = militaryTime.split(" - ");
     const fromTime = splitMilitaryTime[0];
     const toTime = splitMilitaryTime[1];
+
+    const fromTimeSplit = fromTime.split(":");
+    const fromTimeHour = fromTimeSplit[0];
+    const fromTimeMinute = fromTimeSplit[1];
+    const isFromTimeValid = isValidHour(fromTimeHour) && isValidMinute(fromTimeMinute);
     
-    const isFromTimeValid = typeof fromTime === "string";
-    const isToTimeValid = typeof toTime === "string";
+    const toTimeSplit = toTime.split(":");
+    const toTimeHour = toTimeSplit[0];
+    const toTimeMinute = toTimeSplit[1];
+    const isToTimeValid = isValidHour(toTimeHour) && isValidMinute(toTimeMinute);
+    
     return isFromTimeValid && isToTimeValid;
+}
+
+
+function isValidHour(hour: string) {
+    const numberfied = Number(hour);
+
+    return numberfied >= 0 && numberfied <= 23; 
+}
+
+function isValidMinute(minute: string) {
+    const minutefied = Number(minute);
+
+    return minutefied >= 0 && minutefied <= 59; 
 }
