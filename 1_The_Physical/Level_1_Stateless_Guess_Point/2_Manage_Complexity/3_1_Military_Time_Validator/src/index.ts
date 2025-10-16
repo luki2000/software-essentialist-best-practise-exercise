509535eb0f750f70ded81dc75c3ffa075d2080b7
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-16 23:27:48 +0200

    [Refactor] used ai prompt to get rid of repeating code, it simplified and made things more readable

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -3,26 +3,23 @@ export function validateMilitaryTime(militaryTime: string) {
     const fromTime = splitMilitaryTime[0];
     const toTime = splitMilitaryTime[1];
 
-    const fromTimeSplit = fromTime.split(":");
-    const fromTimeHour = fromTimeSplit[0];
-    const fromTimeMinute = fromTimeSplit[1];
-    const isFromTimeValid = isValidHour(fromTimeHour) && isValidMinute(fromTimeMinute);
-    
-    const toTimeSplit = toTime.split(":");
-    const toTimeHour = toTimeSplit[0];
-    const toTimeMinute = toTimeSplit[1];
-    const isToTimeValid = isValidHour(toTimeHour) && isValidMinute(toTimeMinute);
-
-    return isFromTimeValid && isToTimeValid;
+    return validateSingleTime(fromTime) && validateSingleTime(toTime);
 }
 
 
-function isValidHour(h: string) {
-    const hour = Number(h);
+function isValidHour(hour: number) {
     return hour >= 0 && hour <= 23; 
 }
 
-function isValidMinute(m: string) {
-    const minute = Number(m);
+function isValidMinute(minute: number) {
     return minute >= 0 && minute <= 59; 
+}
+
+
+function validateSingleTime(time: string) {
+    const splitTime = time.split(":");
+    const hour = Number(splitTime[0]);
+    const minute = Number(splitTime[1]);
+
+    return isValidHour(hour) && isValidMinute(minute);
 }
