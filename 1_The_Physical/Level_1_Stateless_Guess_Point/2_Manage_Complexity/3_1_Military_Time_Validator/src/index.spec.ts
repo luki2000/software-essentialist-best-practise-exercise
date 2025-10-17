f60de32fc4a79f0d9ef943c1e3745377e86b7e88
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-17 12:26:12 +0200

    [Red] start time should be earlier than end time

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -7,8 +7,12 @@ describe('military time validator', () => {
         { input: "22:00 - 23:12", expected : true },
         { input: "60:00 - 23:90", expected : false },
         { input: "00:00 - 23:59", expected : true },
-    ])('should know if $input is $expected as a valid military time', () => {
-        expect(validateMilitaryTime("25:00 - 12:23")).toBeFalsy();
+    ])('should know if $input is $expected as a valid military time', ({input, expected}) => {
+        expect(validateMilitaryTime(input)).toBe(expected);
+    });
+
+    it('should know that end time being earlier than start time is invalid',() => {
+        expect(validateMilitaryTime("23:00 - 15:30")).toBeFalsy();
     });
 
 });
