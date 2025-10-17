36a1bde890e34cd43c34648c1d2f23cd381035d4
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-17 13:24:02 +0200

    [Red] time range from start to end time wrongly validated when in the same hour range with incompatible minutes

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -15,4 +15,11 @@ describe('military time validator', () => {
         expect(validateMilitaryTime("23:00 - 15:30")).toBeFalsy();
     });
 
+    it.each([
+        { input: "22:00 - 22:00", expected : false },
+        { input: "22:50 - 23:30", expected : false },
+    ])('should know that start time minutes needs to be greater than end time minutes if range is within the same hour',({input, expected}) => {
+        expect(validateMilitaryTime(input)).toBe(expected);
+    });
+
 });
