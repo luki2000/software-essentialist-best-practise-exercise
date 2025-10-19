04bcc6bb238a4985a796d1bb4d9a15ec8a9ac289
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-19 18:29:09 +0200

    [Feature][Red] created boolean calculator class but test fails when evaluating string TRUE

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { BooleanCalculator } from './index';
 
 describe('boolean calculator', () => {
+    const booleanCalculator = new BooleanCalculator()
 
-})
+    it('TRUE as input should return true', () => {
+        expect(booleanCalculator.evaluate("TRUE")).toBeTruthy();
+    });
+});
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,5 @@
+export class BooleanCalculator {
+    evaluate(str: string) {
+
+    }
+}
