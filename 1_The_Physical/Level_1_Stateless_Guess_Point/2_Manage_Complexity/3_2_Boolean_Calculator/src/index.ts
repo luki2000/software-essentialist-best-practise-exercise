c78fafe0c64bab95cce278f3f894def8fe2e1a64
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-28 20:39:24 +0100

    [GREEN] easiest path to deal with not operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,5 +1,12 @@
 export class BooleanCalculator {
     evaluate(str: string) {
+        let tokens = str.split(" ");
+
+        const hasNotOperator = tokens.find((token) => token === "NOT");
+        if(hasNotOperator) {
+            if(tokens[1] === "TRUE") return false;
+            if(tokens[1] === "FALSE") return true;
+        }
         if(str === "TRUE") return true;
         if(str === "FALSE") return false;
         throw Error("not implmemented");
