13aff25214507a3afb7d09a0ac9dbf67777ad12d
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-30 16:42:04 +0100

    [RED] test fails for evaluating OR, not implemented

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -25,4 +25,13 @@ describe('boolean calculator', () => {
     ])('When $input is evaluated it should return $expected', ({input, expected}) => {
         expect(booleanCalculator.evaluate(input)).toBe(expected);
     });
+
+    it.each([
+        {input: 'TRUE OR FALSE', expected: true},
+        {input: 'FALSE OR TRUE', expected: true},
+        {input: 'TRUE OR TRUE', expected: true},
+        {input: 'FALSE OR FALSE', expected: false}    
+    ])('When $input is evaluated it should return $expected', ({input, expected}) => {
+        expect(booleanCalculator.evaluate(input)).toBe(expected);
+    });
 });
