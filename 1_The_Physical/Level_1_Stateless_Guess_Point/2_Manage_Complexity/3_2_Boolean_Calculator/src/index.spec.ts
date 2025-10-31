523e36af500f180a51124cc34fb6e94fee187b1b
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-10-31 14:30:45 +0100

    [RED] handling paranthesis not implemented

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -36,23 +36,25 @@ describe('boolean calculator', () => {
         expect(booleanCalculator.evaluate(input)).toBe(expected);
     });
 
-    it('TRUE AND FALSE OR TRUE should result to true', () => {
-         expect(booleanCalculator.evaluate("TRUE AND FALSE OR TRUE")).toBe(true);
+    it.each([
+        {input: 'TRUE AND FALSE OR TRUE', expected: true},
+        {input: 'TRUE AND FALSE OR FALSE OR TRUE', expected: true},
+        {input: 'NOT TRUE AND TRUE should return false', expected: false}, 
+    ])('When $input is evaluated it should return $expected', ({input, expected}) => {
+        expect(booleanCalculator.evaluate(input)).toBe(expected);
     });
 
-     it('TRUE AND FALSE OR TRUE OR TRUE should result to true', () => {
-         expect(booleanCalculator.evaluate("TRUE AND FALSE OR FALSE OR TRUE")).toBe(true);
+    it.each([
+        {input: 'NOT NOT TRUE', expected: true},
+        {input: 'NOT NOT FALSE', expected: false},
+    ])('it should handle multiple negation', ({input, expected}) => {
+        expect(booleanCalculator.evaluate(input)).toBe(expected);
     });
 
-    it('NOT TRUE AND TRUE should return false', () =>{
-        expect(booleanCalculator.evaluate("NOT TRUE AND TRUE")).toBe(false);
-    });
-
-    it('NOT NOT TRUE should return true', () =>{
-        expect(booleanCalculator.evaluate("NOT NOT TRUE")).toBe(true);
-    });
-
-    it('NOT NOT FALSE should return false', () =>{
-        expect(booleanCalculator.evaluate("NOT NOT FALSE")).toBe(false);
+    it.each([
+        {input: '(TRUE OR TRUE OR TRUE) AND FALSE', expected: false},
+        {input: 'NOT (TRUE AND TRUE)', expected: false},
+    ])('it should handle multiple negation', ({input, expected}) => {
+        expect(booleanCalculator.evaluate(input)).toBe(expected);
     });
 });
