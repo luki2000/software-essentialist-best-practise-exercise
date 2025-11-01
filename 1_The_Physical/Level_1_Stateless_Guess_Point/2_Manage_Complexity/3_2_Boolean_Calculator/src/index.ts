9839daa033784b1065f033410c67fa8120f75f9c
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-11-01 12:45:16 +0100

    [REFACTOR] verify operator presence in one place

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -15,15 +15,11 @@ export class BooleanCalculator {
         if(tokens.length === 1) {
             return this.convertStringToBoolean(str);
         }
-        const hasNOTOperator = tokens.find((token) => token === TokenLibrary.NOT);
-        const hasAndOperator = tokens.find((token) => token === TokenLibrary.AND);
-        const hasOrOperator = tokens.find((token) => token === TokenLibrary.OR);
-        const hasBrackets = tokens.find((token) => token.includes(')'));
 
-        tokens = hasBrackets ? this.processBrackets(tokens) : tokens;
-        tokens = hasNOTOperator ? this.processNotOperator(tokens) : tokens;
-        tokens = hasAndOperator ? this.processAndOperator(tokens) : tokens;
-        tokens = hasOrOperator ? this.processOrOperator(tokens) : tokens;
+        tokens = this.processBrackets(tokens);
+        tokens = this.processNotOperator(tokens);
+        tokens = this.processAndOperator(tokens);
+        tokens = this.processOrOperator(tokens);
 
         return this.convertStringToBoolean(tokens[0]); 
     }
@@ -34,49 +30,55 @@ export class BooleanCalculator {
 
     processNotOperator(tokens: Array<string>): Array<string> {
         let processedTokens = [...tokens];
-        const notOccurence = processedTokens.filter(token => token === TokenLibrary.NOT).length;
-        for(let i= 0; i < notOccurence; i++) {
-            const notOperatorIndex = processedTokens.lastIndexOf(TokenLibrary.NOT);
-            const rightHandSideIndex = notOperatorIndex+1;
-            const rightHandSide = processedTokens[rightHandSideIndex];
+        const hasNOTOperator = tokens.find((token) => token === TokenLibrary.NOT);
+        if(hasNOTOperator) {
+            const notOccurence = processedTokens.filter(token => token === TokenLibrary.NOT).length;
+            for(let i= 0; i < notOccurence; i++) {
+                const notOperatorIndex = processedTokens.lastIndexOf(TokenLibrary.NOT);
+                const rightHandSideIndex = notOperatorIndex+1;
+                const rightHandSide = processedTokens[rightHandSideIndex];
 
-            const result = (rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.FALSE : TokenLibrary.TRUE;
-            processedTokens = processedTokens.slice(0, notOperatorIndex).concat(result, processedTokens.slice(notOperatorIndex + 2));
+                const result = (rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.FALSE : TokenLibrary.TRUE;
+                processedTokens = processedTokens.slice(0, notOperatorIndex).concat(result, processedTokens.slice(notOperatorIndex + 2));
+            }
         }
         return processedTokens;
     }
 
     processAndOperator(tokens: Array<string>): Array<string> {
         let processedTokens = [...tokens];
-        const andOccurence = processedTokens.filter(token => token === TokenLibrary.AND).length;
+        const hasAndOperator = tokens.find((token) => token === TokenLibrary.AND);
+        if(hasAndOperator) {
+            const andOccurence = processedTokens.filter(token => token === TokenLibrary.AND).length;
+            for(let i= 0; i < andOccurence; i++) {
+                const andOperatorIndex = processedTokens.indexOf(TokenLibrary.AND);
+                const leftHandSideIndex = andOperatorIndex-1;
+                const rightHandSideIndex = andOperatorIndex+1;
+                const leftHandSide = processedTokens[leftHandSideIndex];
+                const rightHandSide = processedTokens[rightHandSideIndex];
 
-        for(let i= 0; i < andOccurence; i++) {
-            const andOperatorIndex = processedTokens.indexOf(TokenLibrary.AND);
-            const leftHandSideIndex = andOperatorIndex-1;
-            const rightHandSideIndex = andOperatorIndex+1;
-            const leftHandSide = processedTokens[leftHandSideIndex];
-            const rightHandSide = processedTokens[rightHandSideIndex];
-
-            const result = (leftHandSide === TokenLibrary.TRUE && rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.TRUE : TokenLibrary.FALSE;
-            processedTokens = processedTokens.slice(0, leftHandSideIndex).concat(result, processedTokens.slice(rightHandSideIndex + 1));
+                const result = (leftHandSide === TokenLibrary.TRUE && rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.TRUE : TokenLibrary.FALSE;
+                processedTokens = processedTokens.slice(0, leftHandSideIndex).concat(result, processedTokens.slice(rightHandSideIndex + 1));
+            }
         }
         return processedTokens;
     }
 
     processOrOperator(tokens: Array<string>): Array<string> {
         let processedTokens = [...tokens];
-     
-        const orOccurence = processedTokens.filter(token => token === TokenLibrary.OR).length;
+                    const hasOrOperator = tokens.find((token) => token === TokenLibrary.OR);
+        if(hasOrOperator) {
+            const orOccurence = processedTokens.filter(token => token === TokenLibrary.OR).length;
+            for(let i= 0; i < orOccurence; i++) {
+                const orOperatorIndex = processedTokens.indexOf(TokenLibrary.OR);
+                const leftHandSideIndex = orOperatorIndex-1;
+                const rightHandSideIndex = orOperatorIndex+1;
+                const leftHandSide = processedTokens[leftHandSideIndex];
+                const rightHandSide = processedTokens[rightHandSideIndex];
 
-        for(let i= 0; i < orOccurence; i++) {
-            const orOperatorIndex = processedTokens.indexOf(TokenLibrary.OR);
-            const leftHandSideIndex = orOperatorIndex-1;
-            const rightHandSideIndex = orOperatorIndex+1;
-            const leftHandSide = processedTokens[leftHandSideIndex];
-            const rightHandSide = processedTokens[rightHandSideIndex];
-
-            const result = (leftHandSide === TokenLibrary.TRUE || rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.TRUE : TokenLibrary.FALSE;
-            processedTokens = processedTokens.slice(0, leftHandSideIndex).concat(result, processedTokens.slice(rightHandSideIndex + 1));
+                const result = (leftHandSide === TokenLibrary.TRUE || rightHandSide === TokenLibrary.TRUE) ? TokenLibrary.TRUE : TokenLibrary.FALSE;
+                processedTokens = processedTokens.slice(0, leftHandSideIndex).concat(result, processedTokens.slice(rightHandSideIndex + 1));
+            }
         }
         return processedTokens;
     }
@@ -84,24 +86,20 @@ export class BooleanCalculator {
     processBrackets(tokens: Array<string>): Array<string> {
         let processedTokens = [...tokens];
         const bracketOccurence = processedTokens.filter(token => token.includes(')')).length;
+                const hasBrackets = tokens.find((token) => token.includes(')'));
+        if(hasBrackets) {
+            for(let i= 0; i < bracketOccurence; i++) {
+                const leftPatranthesisIndex =  processedTokens.findIndex((token) => token.includes('('));
+                const rightPatranthesisIndex =  processedTokens.findIndex((token) => token.includes(')'));
+                const elementsInParanthesis = processedTokens.slice(leftPatranthesisIndex, rightPatranthesisIndex+1);
+                let elementsWithoutTheParanthesis = elementsInParanthesis.map(token => token.replace(/^\(|\)$/g, ''));
 
-        for(let i= 0; i < bracketOccurence; i++) {
-            const leftPatranthesisIndex =  processedTokens.findIndex((token) => token.includes('('));
-            const rightPatranthesisIndex =  processedTokens.findIndex((token) => token.includes(')'));
-            const elementsInParanthesis = processedTokens.slice(leftPatranthesisIndex, rightPatranthesisIndex+1);
-            let elementsWithoutTheParanthesis = elementsInParanthesis.map(token => token.replace(/^\(|\)$/g, ''));
-
-
-            const hasNOTOperator = tokens.find((token) => token === TokenLibrary.NOT);
-            const hasAndOperator = tokens.find((token) => token === TokenLibrary.AND);
-            const hasOrOperator = tokens.find((token) => token === TokenLibrary.OR);
-
-            elementsWithoutTheParanthesis = hasNOTOperator ? [...this.processNotOperator(elementsWithoutTheParanthesis)] : elementsWithoutTheParanthesis;
-            elementsWithoutTheParanthesis = hasAndOperator? [...this.processAndOperator(elementsWithoutTheParanthesis)] : elementsWithoutTheParanthesis;
-            elementsWithoutTheParanthesis = hasOrOperator ? [...this.processOrOperator(elementsWithoutTheParanthesis)] : elementsWithoutTheParanthesis;
-            
-            processedTokens = processedTokens.slice(0, leftPatranthesisIndex).concat(elementsWithoutTheParanthesis, processedTokens.slice(rightPatranthesisIndex + 1));
-
+                elementsWithoutTheParanthesis = [...this.processNotOperator(elementsWithoutTheParanthesis)];
+                elementsWithoutTheParanthesis = [...this.processAndOperator(elementsWithoutTheParanthesis)];
+                elementsWithoutTheParanthesis = [...this.processOrOperator(elementsWithoutTheParanthesis)];
+                
+                processedTokens = processedTokens.slice(0, leftPatranthesisIndex).concat(elementsWithoutTheParanthesis, processedTokens.slice(rightPatranthesisIndex + 1));
+            }
         }
         
         return processedTokens;
