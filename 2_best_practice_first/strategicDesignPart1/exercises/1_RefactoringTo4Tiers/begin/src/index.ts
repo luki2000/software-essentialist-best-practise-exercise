b33f14428ac6fc4363a39e56ba9b6103e1653fcb
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2025-11-15 21:43:25 +0100

    first commit

--- 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
+++ 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// 1 first commit
 const Errors = {
     ValidationError: 'ValidationError',
     StudentNotFound: 'StudentNotFound',
